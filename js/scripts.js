fe374e7fba3ba52d3388ba11831134094cbeb53b
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-17 16:04:24 +0200

    creato lo script e collegato al dom

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,39 @@
-const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
-
-let imageArray = [];
+const API_URL = "https://lanciweb.github.io/demo/api/pictures/";
+const photoListElement = document.querySelector("#photo-list");
 
 fetch(API_URL)
-    .then(response => response.json())
-    .then(jsonData => {
-        console.log(jsonData);
-        imageArray = jsonData.map(image => image.url);
-        console.log(imageArray);
+    .then(function (response) {
+        return response.json();
     })
-    .catch(error => {
-        console.error(error);
+    .then(function (photos) {
+        printPhotos(photos);
+    })
+    .catch(function (error) {
+        console.error("Errore API:", error);
+        photoListElement.innerHTML = "<p class='text-white text-center'>Errore nel caricamento delle foto</p>";
     });
+
+function printPhotos(photos) {
+    photoListElement.innerHTML = "";
+
+    for (let i = 0; i < photos.length; i++) {
+        const photo = photos[i];
+
+        const cardHtml = `
+            <div class="col d-flex justify-content-center">
+                <article class="card border-0 rounded-0 photo-card">
+                    <div class="pin"></div>
+                    <div class="card-body p-3">
+                        <img src="${photo.url}" class="img-fluid" alt="${photo.title}">
+                        <div class="pt-3">
+                            <p class="photo-date">${photo.date}</p>
+                            <p class="photo-title">${photo.title}</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        `;
+
+        photoListElement.innerHTML += cardHtml;
+    }
+}
